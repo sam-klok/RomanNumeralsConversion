aa11605b59d7715c47ccb58cdb32b1a5c97cbff4
sam-klok <sklok11@gmail.com>
2022-06-09 09:51:38 -0700

    Initial commit

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Fun exercise with Roman numerals.
+Math teachers in ... 5th grad may ask, how to add/subtract them. It's actually not that obvious.
+
+
+Example based on:
+https://stackoverflow.com/questions/7040289/converting-integers-to-roman-numerals
+
---
+++ RomanNumberalsConversion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumberalsConversion
+{
+    internal class Program
+    {
+        private static Dictionary<char, int> Romans = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 },
+        };
+
+        static int convertRomanToInt(string s)
+        {
+            int n = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && Romans[s[i]] < Romans[s[i + 1]])
+                {
+                    n -= Romans[s[i]];
+                }
+                else
+                {
+                    n += Romans[s[i]];
+                }
+            }
+            return n;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "IVI";
+
+            Console.WriteLine($"{s} = {convertRomanToInt(s)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
